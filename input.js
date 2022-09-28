7cf563900728661fb347a4815359452bb712154d
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-09-28 20:48:47 +0000

    Added connection object to set input so that you're control of the snake can feed directly to the servers

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +13,15 @@ const setupInput = function () {
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
+  } else if (data === "w") {
+    connection.write("Move: up");
+  } else if (data === "a") {
+    connection.write("Move: left");
+  } else if (data === "s") {
+    connection.write("Move: down");
+  }
+  else if (data === "d") {
+    connection.write("Move: right");  
   }
 };
 
